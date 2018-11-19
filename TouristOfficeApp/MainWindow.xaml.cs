4c43a58c4d800fab3c00ba74e502f7b7accba6e2
VyacheslavTretyak <tretyak1c@gmail.com>
2018-11-19 11:42:19 +0200

    create project

--- TouristOfficeApp/MainWindow.xaml.cs
+++ TouristOfficeApp/MainWindow.xaml.cs
@@ -14,10 +14,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace TouristOfficeApp
-{
-	/// <summary>
-	/// Interaction logic for MainWindow.xaml
-	/// </summary>
+{	
 	public partial class MainWindow : Window
 	{
 		public MainWindow()
